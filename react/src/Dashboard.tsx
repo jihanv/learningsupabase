15c0fea54940bf0d31470c9fd48c8ca9e5036fe9
jihanv <61974348+jihanv@users.noreply.github.com>
2026-01-24 18:17:45 +0900

    Format data for chart

--- react/src/Dashboard.tsx
+++ react/src/Dashboard.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import supabase from "./supabase-client";
+import { Chart } from "react-charts";
 
 type Metric = { name: string; sum: number };
+type ChartDataPoint = { primary: string; secondary: number };
 
 function Dashboard() {
 
@@ -30,14 +32,62 @@ function Dashboard() {
         }
     }
 
+    const chartData = [
+        {
+            data: metrics.map((metric) => ({
+                primary: metric.name,
+                secondary: metric.sum
+            }))
+        }
+    ]
     useEffect(() => {
         fetchMetrics()
     }, [])
+    function y_max() {
+        if (metrics.length > 0) {
+            const maxSum = Math.max(...metrics.map((m) => m.sum));
+            return maxSum + 2000;
+        }
+        return 5000;
+    }
+    const primaryAxis = {
+        getValue: (d: ChartDataPoint) => d.primary,
+        scaleType: 'band' as const,
+        padding: 0.2,
+        position: 'bottom' as const,
+    };
+    const secondaryAxes = [
+        {
+            getValue: (d: ChartDataPoint) => d.secondary,
+            scaleType: 'linear' as const,
+            elementType: "bar" as const,
+            min: 0,
+            max: y_max(),
+            padding: {
+                top: 20,
+                bottom: 40,
+            },
+        },
+    ];
     return (
         <div className="dashboard-wrapper">
             <div className="chart-container">
                 <h2>Total Sales This Quarter ($)</h2>
+                <div style={{ flex: 1 }}>
+                    <Chart
+                        options={{
+                            data: chartData,
+                            primaryAxis,
+                            secondaryAxes,
+                            defaultColors: ['#58d675'],
+                            tooltip: {
+                                show: false,
+                            },
+                        }}
+                    />
+                </div>
             </div>
+
         </div>
     );
 }
